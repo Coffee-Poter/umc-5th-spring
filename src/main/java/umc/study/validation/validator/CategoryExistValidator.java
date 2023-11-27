c0b7d01a430049861dccffbe5e9ee29e9d844e42
Coffee-Poter <113098614+Coffee-Poter@users.noreply.github.com>
2023-11-27 14:47:20 +0900

    merge feature/1 to main

---
+++ src/main/java/umc/study/validation/validator/CategoryExistValidator.java
@@ -0,0 +1,36 @@
+package umc.study.validation.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.service.MemberService.MemberCommandServiceImpl;
+import umc.study.validation.anotation.ExistCategories;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoryExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
+
+    private final MemberCommandServiceImpl memberCommandService;
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        boolean isValid = values.stream()
+                .allMatch(value -> memberCommandService.findFoodCategoryById(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+
+    }
+}
